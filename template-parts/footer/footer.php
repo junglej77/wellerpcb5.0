5f11fda82802b0f7d2a39721b6188857da6a7ebf
jungle77 <junglej77>
2023-06-01 20:13:05 +0800

    将发送邮件的前端样式删除

--- template-parts/footer/footer.php
+++ template-parts/footer/footer.php
@@ -83,42 +83,6 @@
 
 <!-- Scroll_To_Top -->
 <div class="scroll-to-top scroll-to-target" data-target="html"><span class="fa fa-angle-up"></span></div>
-
-
-<!-- 邮箱采集 -->
-<div class="dialog_popup_Email hidden">
-  <div class="dialog_content_wrap">
-    <div class="dialog_content wow" data-wow-duration="2s">
-      <span class="dialog_close">x</span>
-      <div class="dialog-header">Request A Quote</div>
-      <div class="dialog-message">
-        <div class="formItem formItem_inline">
-          <input id="from_email" type="text" placeholder="Email">
-        </div>
-        <div class="formItem formItem_inline">
-          <input id="from_name" type="text" placeholder="Full Name">
-        </div>
-        <div class="formItem formItem_inline">
-          <input id="from_name" type="text" placeholder="Phone">
-        </div>
-        <div class="formItem formItem_inline">
-          <input id="from_name" type="text" placeholder="Subject">
-        </div>
-        <div class="formItem">
-          <textarea id="email_body" type="textarea" rows="4" name="message" placeholder="What can we help?"></textarea>
-        </div>
-        <div class="formItem file">
-          <div class="input_title">Upload Your Design</div>
-          <input id="email_attachment" type="file" name="Attachment">
-        </div>
-        <div class="formItem submit">
-          <input id="sendEmail" type="submit" value=" Send Your Inquiry">
-        </div>
-      </div>
-      <h4 class="info">*We respect your confidentiality and all information are protected.</h4>
-    </div>
-  </div>
-</div>
 <?php wp_footer(); ?>
 </body>
 
